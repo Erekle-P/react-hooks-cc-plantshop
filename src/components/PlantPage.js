88b13e27bbb178eb68319975199b5e0792f6b571
Erekle Papuashvili <erekle.papuashvili@gmail.com>
2024-12-19 16:49:22 -0500

    app filters plants by name, adds new plants and changes 'in stock' / 'out of stock' after click

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, setPlants }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search onSearch={handleSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
