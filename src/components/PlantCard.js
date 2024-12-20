c6ba2ea735497ff9740bbc94995f0bec9794e628
Erekle Papuashvili <erekle.papuashvili@gmail.com>
2024-12-20 13:01:46 -0500

    remove delete button

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 function PlantCard({ plant, onDelete }) {
-  const handleDelete = () => {
-    onDelete(plant.id);
-  };
+  // const handleDelete = () => {
+  //   onDelete(plant.id);
+  // };
 
   return (
     <li className="card" data-testid="plant-item">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      <button onClick={handleDelete}>Delete</button>
+      {/* <button onClick={handleDelete}>Delete</button> */}
       {plant.isSoldOut ? (
         <button>Out of Stock</button>
       ) : (
